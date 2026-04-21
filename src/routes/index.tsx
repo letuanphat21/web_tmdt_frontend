63818e03e30a8ffa2041cb26d62bae1737f1f7a6
Le Tuan Phat <ltphat240103@gmail.com>
2026-04-21 16:03:47 +0700

    cấu hình routes hoàn tất

---
+++ src/routes/index.tsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router-dom";
+import UserLayout from "../layouts/UserLayout";
+import publicRoutes from "./publicRoutes";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <UserLayout />,
+    children: publicRoutes,
+  },
+]);
+export default router;
