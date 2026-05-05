d69d9f7c1e0abb5bcbee3b114dfb617c6e44ec05
Le Tuan Phat <ltphat240103@gmail.com>
2026-05-05 11:08:21 +0700

    completed sideBarUser

--- src/components/common/ORDivider.tsx
+++ src/components/common/ORDivider.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
+import React from "react";
 
 interface ORDividerProps {
-    text: string;
+  text: string;
 }
 
 const ORDivider: React.FC<ORDividerProps> = ({ text }) => {
-    return (
-        <div className="relative flex flex-col justify-center items-center py-[16px] w-full h-[48px]">
-            {/* Đường gạch ngang */}
-            <div className="absolute inset-0 flex items-center justify-center pointer-events-none">
-                <div className="w-full border-t border-brand-divider"></div>
-            </div>
-            {/* Chữ Text đè lên đường gạch */}
-            <div className="relative z-10 flex flex-col items-center px-[16px] bg-brand-bg h-[16px]">
-        <span className="font-bold text-[12px] leading-[16px] tracking-[1.2px] uppercase text-brand-social-text">
+  return (
+    <div className="relative flex flex-col justify-center items-center py-4 w-full h-12">
+      {/* Đường gạch ngang */}
+      <div className="absolute inset-0 flex items-center justify-center pointer-events-none">
+        <div className="w-full border-t border-brand-divider"></div>
+      </div>
+      {/* Chữ Text đè lên đường gạch */}
+      <div className="relative z-10 flex flex-col items-center px-4 bg-brand-bg h-4">
+        <span className="font-bold text-[12px] leading-4 tracking-[1.2px] uppercase text-brand-social-text">
           {text}
         </span>
-            </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ORDivider;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,28 @@
 import { createBrowserRouter } from "react-router-dom";
 import UserLayout from "../layouts/UserLayout";
 import publicRoutes from "./publicRoutes";
+import ProfileLayout from "@/layouts/ProfileLayout/ProfileLayout";
+import ProfileRoute from "./ProfileRoute";
+import profileRoutes from "./profileRoutes";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <UserLayout />,
-        children: publicRoutes,
-    },
+  {
+    path: "/",
+    element: <UserLayout />,
+    children: [
+      ...publicRoutes,
+      {
+        element: <ProfileRoute />,
+        children: [
+          {
+            path: "profile",
+            element: <ProfileLayout />,
+            children: profileRoutes,
+          },
+        ],
+      },
+    ],
+  },
 ]);
 
 export default router;
