5edec78a1fe1c6035fa791626fd9e68b05c80a11
qthong206 <22130275@st.hcmuaf.edu.vn>
2026-05-06 21:53:57 +0700

    feat: thêm giao diện trang chi tiết sản phẩm và cập nhật route

--- src/routes/publicRoutes.tsx
+++ src/routes/publicRoutes.tsx
@@ -2,6 +2,7 @@ import Home from "@/pages/Home";
 import Login from "@/pages/Login";
 import Register from "@/pages/Register";
 import Checkout from "@/pages/Checkout";
+import ProductDetail from "@/pages/ProductDetail";
 
 const publicRoutes = [
     {
@@ -19,6 +20,10 @@ const publicRoutes = [
     {
         path: "/checkout",
         element: <Checkout />
+    },
+    {
+        path: "/product/:id",
+        element: <ProductDetail />
     }
 ];
 
